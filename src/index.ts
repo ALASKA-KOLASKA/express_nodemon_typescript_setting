eee3bee8b00be365c2896e82816ab5e1c9b8e73f
ALASKA_KOLASKA <thefreshchannel@mail.ru>
2022-12-05 23:15:55 +0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+
+const app = express()
+const port = 3003
+
+app.get('/', (req, res) => {
+    res.send({message: 'Hello World!'})
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
